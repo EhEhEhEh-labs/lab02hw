895fef778db548948e04af4fc192518ae95c46fa
EhEhEhEh-labs <new3now@yandex.ru>
2025-05-19 20:41:00 +0000

    REAL GOOD STYLE

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
 
-int main() 
-{
-  string name;
-  cin >> name;
-  cout << "Hello world from " << name << endl;
+int main() {
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
   return 0;
 }
